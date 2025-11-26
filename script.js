6076617c7bbad333718b11c0032bb15a3537dfa6
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2025-11-26 06:15:02 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-//your JS code here. If required.
 const inputs = document.querySelectorAll(".code");
 
-        inputs[0].focus();
+    inputs.forEach((input, index) => {
 
-        inputs.forEach((input, index) => {
-            input.addEventListener("input", (e) => {
-                const value = e.target.value;
+      input.addEventListener("input", (e) => {
+        let value = e.target.value;
 
-                if (value.length === 1 && index < inputs.length - 1) {
-                    inputs[index + 1].focus();
-                }
-            });
+        // allow only digits
+        if (!/^[0-9]$/.test(value)) {
+          e.target.value = "";
+          return;
+        }
 
-            input.addEventListener("keydown", (e) => {
-                if (e.key === "Backspace") {
-                    if (input.value === "" && index > 0) {
-                        inputs[index - 1].focus();
-                        inputs[index - 1].value = "";
-                    }
-                }
-            });
-        });
+        // move forward
+        if (value && index < inputs.length - 1) {
+          inputs[index + 1].focus();
+        }
+      });
+
+      input.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace") {
+          // if empty, move backward
+          if (input.value === "" && index > 0) {
+            inputs[index - 1].focus();
+            inputs[index - 1].value = "";
+          }
+        }
+      });
+    });
